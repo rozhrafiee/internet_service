1dd7c08631f0726caa8d747d3e42f221914b382d
rozhrafiee <rozhinarafiee014@gmail.com>
2024-08-29 11:29:10 +0430

    commit1

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_provider = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
-    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- service/models.py
+++ service/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Service(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -6,3 +8,11 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+
+class PurchasedService(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} bought {self.service.name}"
